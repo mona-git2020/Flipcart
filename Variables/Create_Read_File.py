3104e7e1834cd43b1f11dcbb4e5b42d5fdfde99c
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-10-22 21:56:34 +0530

    write script for TC6 and try to get data from ppt file

--- Variables/Create_Read_File.py
+++ Variables/Create_Read_File.py
@@ -1,6 +1,25 @@
+from pptx import Presentation
+
 def create_new_file(file_name, content):
 	file = open(file_name, "w+")
 	file.write(content)
 	file = open(file_name, "r")
 	file_Content = file.read()
 	return file_Content
+	
+def open_and_read_file(file_name):
+	print("hey")
+	file = open(file_name, "r")
+	print("hey.....Hi...")
+	prs = Presentation(file_name)
+	print("hey.....Hi...I m Good....")
+	fileDataList = []
+	print(fileDataList)
+	for slide in prs.slides:
+		print("First For Loop")
+		for shape in slide.shapes:
+			print("First For Loop")
+			print(shape.text)
+			fileDataList.append(shape.text)
+				
+	
---
+++ Variables/Variables_FileExamples.py
@@ -0,0 +1,4 @@
+urlOfFileExample = "https://file-examples.com/index.php/sample-documents-download/sample-ppt-file/"
+
+profileAddress = "C:/Users/AMAN/AppData/Roaming/Mozilla/Firefox/Profiles/90yqdr7b.default"
+downloadDir = "D:/MONU/Robotframework/Intranet/Downloads"
