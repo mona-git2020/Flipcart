3446dc03de3942281992b4d2b69cfc83c1983eb3
Monalisha Prajapati <Monalisha.Prajapati@einfochips.com>
2020-12-08 18:16:14 +0530

    TC8(Set Library And Verify) is completed

---
+++ Variables/SetLibraryCustomKeywordFile1.py
@@ -0,0 +1,3 @@
+def Mathematics(value1, value2):
+	sum = float(value1) + float(value2)
+	return sum
---
+++ Variables/SetLibraryCustomKeywordFile2.py
@@ -0,0 +1,3 @@
+def Mathematics(value1, value2):
+	sub = float(value1) - float(value2)
+	return sub
